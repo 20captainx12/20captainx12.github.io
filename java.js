f2e2e7e91da11ee0fa051c74f45bd854bc8a43bf
20captainx12 <63267239+20captainx12@users.noreply.github.com>
2020-04-07 00:17:23 +0100

    Create java.js

---
+++ java.js
@@ -0,0 +1,64 @@
+function UOPew() {
+  var x = document.getElementById("UOPew");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function jTkiMN() {
+  var x = document.getElementById("jTkiMN");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function ZAjiIYB() {
+  var x = document.getElementById("ZAjiIYB");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function joS() {
+  var x = document.getElementById("joS");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function EcRmGD() {
+  var x = document.getElementById("EcRmGD");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function TrbQf() {
+  var x = document.getElementById("TrbQf");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function xqiGf() {
+  var x = document.getElementById("xqiGf");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function KUELgI() {
+  var x = document.getElementById("KUELgI");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
